72faeead9027e079a710b248c1c7c7c8e9086431
kouuch <rifkyirsada2@gmail.com>
2025-05-09 20:53:19 +0800

    Menambahkan styling untuk kategori dengan box dan ikon interaktif
    Menerapkan flexbox pada elemen .box untuk tata letak vertikal, memberikan padding, border radius, dan menambahkan gambar responsif. Menambahkan ikon dengan tampilan tersembunyi yang muncul saat hover pada kategori.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 var swiper = new Swiper(".home", {
     spaceBetween: 30,
     centeredSlides: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
