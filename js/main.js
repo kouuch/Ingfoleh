11f84635a94924e6bde0b850cade5c2dd3730056
kouuch <rifkyirsada2@gmail.com>
2025-05-18 19:30:02 +0800

    Submenu Profil dengan Toggle Animasi dan Custom Scrollbar  Deskripsi:
    Proyek ini menghadirkan submenu profil interaktif dengan efek buka-tutup menggunakan animasi transisi CSS dan toggle ikon panah menggunakan JavaScript. Selain itu, dilengkapi juga dengan custom scrollbar yang memperindah tampilan UI agar lebih modern dan responsif.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,36 @@
 // swiper
 var swiper = new Swiper(".home", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
 function toggleHeart(element) {
-    // Menambah atau menghapus class 'liked' saat icon diklik
-    if (element.classList.contains('bx-heart')) {
-      element.classList.remove('bx-heart');
-      element.classList.add('bxs-heart');
-    } else {
-      element.classList.remove('bxs-heart');
-      element.classList.add('bx-heart');
-    }
+  if (element.classList.contains('bx-heart')) {
+    element.classList.remove('bx-heart');
+    element.classList.add('bxs-heart');
+  } else {
+    element.classList.remove('bxs-heart');
+    element.classList.add('bx-heart');
   }
+}
+
+let subMenu = document.getElementById("subMenu");
+
+function toggleMenu(element) {
+  if (element.classList.contains('bx-caret-down')) {
+    element.classList.remove('bx-caret-down');
+    element.classList.add('bx-caret-up');
+  } else {
+    element.classList.remove('bx-caret-up');
+    element.classList.add('bx-caret-down');
+  }
+  subMenu.classList.toggle("open-menu");
+}
