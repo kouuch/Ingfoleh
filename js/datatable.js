9c743973f6e660bf1f678d48e9f7f5f05ff05e6c
kouuch <rifkyirsada2@gmail.com>
2025-05-21 22:17:38 +0800

    Tambah fitur modal form untuk tambah produk baru
    Menambahkan modal form interaktif dengan validasi input untuk menambah data produk baru, termasuk field nama produk, kategori, lokasi, kontak, kisaran harga, dan upload gambar. Juga menyertakan styling CSS untuk tampilan tombol, modal, dan tabel, serta menginisialisasi DataTables dengan opsi bahasa dan scrolling vertikal yang responsif.

--- js/datatable.js
+++ js/datatable.js
@@ -1,1 +1,39 @@
-new DataTable('#example');
+document.addEventListener('DOMContentLoaded', function () {
+    $('#example').DataTable({
+        destroy: true,
+        language: {
+            lengthMenu: 'Tampilkan _MENU_ entri per halaman',
+            zeroRecords: 'Tidak ada data yang cocok',
+            info: 'Menampilkan _START_ sampai _END_ dari _TOTAL_ entri',
+            infoEmpty: 'Menampilkan 0 sampai 0 dari 0 entri',
+            infoFiltered: '(difilter dari total _MAX_ entri)'
+        },
+        scrollY: '600px',
+        scrollCollapse: true
+    });
+
+    var modalForm
+    document.getElementById('addNewBtn').addEventListener('click', function () {
+        modalForm = new bootstrap.Modal(document.getElementById('exModal'), {
+            backdrop: 'static',
+            keyboard: false,
+        })
+        modalForm.show()
+    })
+
+    document.getElementById('close').addEventListener('click', function () {
+        window.location.href = '../../index.html'
+    })
+
+    // document.querySelectorAll('input[required]').forEach(input => {
+    //     input.oninvalid = function (e) {
+    //         e.target.setCustomValidity('Harap isi kolom ini');
+    //     };
+    //     input.oninput = function (e) {
+    //         e.target.setCustomValidity('');
+    //     };
+    // });
+})
+
+
+
